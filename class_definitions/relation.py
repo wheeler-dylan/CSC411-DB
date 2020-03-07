ba9d63fd5def6ec93a2ac8cf558605e4865a5e64
Dylan Wheeler <dylan.wheeler@usm.edu>
2020-03-06 18:48:16 -0600

    get_query() sql query builder for relation class built and functions in sandbox, next step is to update to main.py and convert product_tables.py to csv files in schemas

--- class_definitions/relation.py
+++ class_definitions/relation.py
@@ -9,12 +9,30 @@ class Relation:
 
     def __init__(self, f_file): #f_file is a formatted csv file found in shcemas folder
         self.name = str(f_file.replace(str(".\\schemas\\"), "").replace(".csv", ""))
-        self.file = f_file
-        self.attributes = open(self.file).readline().split(",")
+        self.file = open(f_file)
+        self.attributes = self.file.readline().split(";") #header names
+        self.filetypes = self.file.readline().split(";")  #sql calls for datatypes
     #
 
     def print_me(self):
         print(self.name)
         print(self.attributes)
+        print(self.filetypes)
         print("\n\n\n")
     #
+
+    def get_query(self):
+        query = str("create table " + self.name + " (")
+        for i in range(len(self.attributes)):
+            query = query + (str(self.attributes[i] + " " + self.filetypes[i]))
+            if i == 0:
+                query = query + " primary key"
+            if i != (len(self.attributes)-1):
+                query = query + ", "
+        query = query + ");"
+
+        return query
+    #
+
+#
+
